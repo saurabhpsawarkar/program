80313ee63d3254b2b75f447077da060e6657f461
saurabh <saurabh@12>
2023-07-29 13:36:19 +0530

    new changes

--- server.py
+++ server.py
@@ -2,9 +2,11 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/", methods=["GET"])
 def root():
-    return "welcome to ITIL exam"
+    return "welcome to ITIL exam.............."
+@app.route("/modules", methods=["GET"])
 def modules():
     return "COSA,ITIAM,FCN,NDC,security-concepts,dev-ops,compliance-audit"
+@app.route("/me", methods=["GET"])
 def me():
     return "PRN:230344223045,Name: saurabh sawarkar,Phone number: 960415574"
       
